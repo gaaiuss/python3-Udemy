92b94c7c5771caaff7d0b972c1b2fa7d1979930c
gaaiuss <caio.gui.castro@gmail.com>
2025-07-16 11:51:08 -0300

    finished

--- merge_lists.py
+++ merge_lists.py
@@ -8,3 +8,15 @@
 # ['BA', 'SP', 'MG', 'RJ']
 # Resultado
 # [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
+
+# def zipper(list1, list2):
+#     interval = min(len(list1), len(list2))
+#     return [(list1[i], list2[i]) for i in range(interval)]
+from itertools import zip_longest
+
+
+list1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+list2 = ['BA', 'SP', 'MG', 'RJ']
+
+print(list(zip(list1, list2)))
+print(list(zip_longest(list1, list2, fillvalue='NO CITY')))
