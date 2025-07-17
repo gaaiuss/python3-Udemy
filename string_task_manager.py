5768e945b5a0621e1f0642269391b5058658aa6c
gaaiuss <caio.gui.castro@gmail.com>
2025-07-17 15:42:58 -0300

    partial solution

---
+++ string_task_manager.py
@@ -0,0 +1,42 @@
+# Exercício - Lista de tarefas com desfazer e refazer
+# Música para codar =)
+# Everybody wants to rule the world - Tears for fears
+# todo = [] -> lista de tarefas
+# todo = ['fazer café'] -> Adicionar fazer café
+# todo = ['fazer café', 'caminhar'] -> Adicionar caminhar
+# desfazer = ['fazer café',] -> Refazer ['caminhar']
+# desfazer = [] -> Refazer ['caminhar', 'fazer café']
+# refazer = todo ['fazer café']
+# refazer = todo ['fazer café', 'caminhar']
+from os import system
+import json
+
+
+def validate_index(list):
+    try:
+        return list.pop()
+    except IndexError:
+        print('No values to pop\n')
+
+
+answer = ''
+todo = []
+redo = []
+
+while answer != '5':
+    answer = input(
+        'Type a task or choose one action:\n1- list\n2 - undo\n3 - redo\n4 - clear terminal\n5 - exit\n\n')
+    print()
+
+    if answer not in ('1', '2', '3', '4', '5'):
+        todo.append(answer)
+    elif answer == '1':
+        print(todo, '\n') if todo else print('Empty list\n')
+    elif answer == '2':
+        redo.append(validate_index(todo))
+        print(todo, '\n')
+    elif answer == '3':
+        todo.append(validate_index(redo))
+        print(todo, '\n')
+    elif answer == '4':
+        system('cls')
