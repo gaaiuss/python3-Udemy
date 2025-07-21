56a16bad44422768c68a9f9b723c5d36d0aa51c2
gaaiuss <caio.gui.castro@gmail.com>
2025-07-21 14:19:41 -0300

    creating my abstract method

---
+++ heritage_mixins_abstraction/log.py
@@ -0,0 +1,39 @@
+# Abstraction
+# Log
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
+
+class Log:
+    def _log(self, msg):
+        raise NotImplementedError('Implement the log method')
+
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
+
+
+class LogFileMixin(Log):
+    def _log(self, msg):
+        formatted_msg = f'{msg} ({self.__class__.__name__})'
+        print(f'Saving log {formatted_msg}')
+        with open(LOG_FILE, 'a', encoding='utf8') as archive:
+            archive.write(formatted_msg)
+            archive.write('\n')
+
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+
+
+if __name__ == '__main__':
+    lp = LogPrintMixin()
+    lp.log_error('\'Ello there!')
+    lp.log_success('Nice!')
+    lf = LogFileMixin()
+    lf.log_error('\'Ello there!')
+    lf.log_success('Nice!')
